1986cda5ff834666602fe63a0cf96e89f6ad5b1e
Ryan Thompson <service@aiwebsystems.com>
2014-10-31 01:42:01 -0500

    Add blocked attribute

--- src/Installer/UsersFieldInstaller.php
+++ src/Installer/UsersFieldInstaller.php
@@ -37,6 +37,9 @@ class UsersFieldInstaller extends FieldInstaller
         'last_name'        => [
             'type' => 'text',
         ],
+        'is_blocked'       => [
+            'type' => 'boolean',
+        ],
         'last_activity_at' => [
             'type' => 'datetime',
         ],
--- src/Installer/UsersStreamInstaller.php
+++ src/Installer/UsersStreamInstaller.php
@@ -22,6 +22,7 @@ class UsersStreamInstaller extends StreamInstaller
         'email'            => ['is_required' => true],
         'username'         => ['is_required' => true],
         'password'         => ['is_required' => true],
+        'is_blocked'       => [],
         'permissions'      => [],
         'first_name'       => [],
         'last_name'        => [],
