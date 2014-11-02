9049656c2caaaf23c752461683175f4ee95fc4cd
Ryan Thompson <service@aiwebsystems.com>
2014-11-01 22:03:00 -0500

    Testing

---
+++ resources/lang/fr/field.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'first_name'       => [
+        'name'         => 'French File',
+        'instructions' => 'What is the users first name in french?',
+        'placeholder'  => 'Johnwah',
+    ],
+    'last_name'        => [
+        'name'         => 'French Last Name',
+        'instructions' => 'What is the users last name in french?',
+        'placeholder'  => 'Doemeo',
+    ],
+];
