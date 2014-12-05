a6f43a2de3f0368ce835486a8a0d0e8e8df1b5a3
Ryan Thompson <service@aiwebsystems.com>
2014-12-05 01:11:34 -0600

    Testing

--- src/Ui/Form/UserForm.php
+++ src/Ui/Form/UserForm.php
@@ -1,7 +1,6 @@
 <?php namespace Anomaly\Streams\Addon\Module\Users\Ui\Form;
 
-use Anomaly\Streams\Addon\Module\Users\User\UserModel;
-use Anomaly\Streams\Platform\Ui\Form\Form;
+use Anomaly\Streams\Platform\Ui\Form\FormBuilder;
 
 /**
  * Class UserForm
@@ -11,111 +10,23 @@ use Anomaly\Streams\Platform\Ui\Form\Form;
  * @author        Ryan Thompson <ryan@anomaly.is>
  * @package       Anomaly\Streams\Addon\Module\Users\Ui
  */
-class UserForm extends Form
+class UserForm extends FormBuilder
 {
 
-    /**
-     * Setup the form class.
-     */
-    protected function boot()
-    {
-        $this->setUpModel();
-        $this->setUpSkips();
-        $this->setUpButtons();
-        $this->setUpActions();
-        $this->setUpIncludes();
-        $this->setUpSections();
-    }
+    protected $model = 'Anomaly\Streams\Addon\Module\Users\User\UserModel';
 
-    /**
-     * Set up the model.
-     */
-    protected function setUpModel()
-    {
-        $this->setModel(new UserModel());
-    }
+    protected $sections = [
+        [
+            'layout' => 'Test HTML'
+        ]
+    ];
 
-    /**
-     * Set up the skipped fields.
-     */
-    protected function setUpSkips()
-    {
-        $this->setSkips(
-            [
-                'persistence_code',
-                'last_activity_at',
-                'activation_code',
-                'last_login_at',
-                'is_activated',
-                'activated_at',
-                'reset_code',
-                'is_blocked',
-                'blocked_at',
-            ]
-        );
-    }
+    protected $actions = [
+        'save',
+    ];
 
-    /**
-     * Set up form actions.
-     */
-    protected function setUpActions()
-    {
-        $this->setActions(
-            [
-                'save',
-            ]
-        );
-    }
-
-    /**
-     * Set up the form buttons.
-     */
-    protected function setUpButtons()
-    {
-        $this->setButtons(
-            [
-                'cancel',
-                'delete',
-            ]
-        );
-    }
-
-    /**
-     * Set up the form sections.
-     */
-    protected function setUpSections()
-    {
-        $this->setSections(
-            [
-                [
-                    'type'   => 'default',
-                    'title'  => 'User Information',
-                    'fields' => [
-                        'username',
-                        'email',
-                        'roles',
-                        'password',
-                        'password_confirmation' => [
-                            'label'        => 'module.users::field.password_confirmation.label',
-                            'instructions' => 'module.users::field.password_confirmation.instructions',
-                            'type'         => 'text',
-                        ]
-                    ]
-                ]
-            ]
-        );
-    }
-
-    /**
-     * Set up included non-stream fields.
-     */
-    protected function setUpIncludes()
-    {
-        $this->setInclude(
-            [
-                'password_confirmation',
-            ]
-        );
-    }
+    protected $buttons = [
+        'edit' => 'Testing',
+    ];
 }
  
--- src/Ui/Table/UserTable.php
+++ src/Ui/Table/UserTable.php
@@ -1,11 +1,6 @@
 <?php namespace Anomaly\Streams\Addon\Module\Users\Ui\Table;
 
-use Anomaly\Streams\Addon\Module\Users\Activation\Contract\ActivationInterface;
-use Anomaly\Streams\Addon\Module\Users\Block\Contract\BlockInterface;
-use Anomaly\Streams\Addon\Module\Users\User\Contract\UserInterface;
-use Anomaly\Streams\Addon\Module\Users\User\UserModel;
-use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
-use Anomaly\Streams\Platform\Ui\Table\Table;
+use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
 
 /**
  * Class UserTable
@@ -15,169 +10,37 @@ use Anomaly\Streams\Platform\Ui\Table\Table;
  * @author        Ryan Thompson <ryan@anomaly.is>
  * @package       Anomaly\Streams\Addon\Module\Users\Ui
  */
-class UserTable extends Table
+class UserTable extends TableBuilder
 {
 
-    /**
-     * Set up the table class.
-     */
-    protected function boot()
-    {
-        $this->setUpModel();
-        $this->setUpViews();
-        $this->setUpFilters();
-        $this->setUpColumns();
-        $this->setUpButtons();
-        $this->setUpActions();
-    }
+    protected $model = 'Anomaly\Streams\Addon\Module\Users\User\UserModel';
 
-    /**
-     * Set up the model.
-     */
-    protected function setUpModel()
-    {
-        $this
-            ->setModel(new UserModel())
-            ->setEager(['activation', 'block']);
-    }
+    protected $views = [
+        'view_all' => [
+            'test' => 'foo',
+            'view' => 'all',
+        ],
+    ];
 
-    /**
-     * Set up the table views.
-     */
-    protected function setUpViews()
-    {
-        $this->setViews(
-            [
-                'all',
-                [
-                    'slug'    => 'active',
-                    'title'   => 'module.users::ui.active',
-                    'handler' => 'Anomaly\Streams\Addon\Module\Users\Ui\Table\View\ActiveUsersTableView',
-                ],
-                [
-                    'slug'    => 'inactive',
-                    'title'   => 'module.users::ui.inactive',
-                    'handler' => 'Anomaly\Streams\Addon\Module\Users\Ui\Table\View\InactiveUsersTableView',
-                ],
-                [
-                    'slug'    => 'blocked',
-                    'title'   => 'module.users::ui.blocked',
-                    'handler' => 'Anomaly\Streams\Addon\Module\Users\Ui\Table\View\BlockedUsersTableView',
-                ],
+    protected $filters = [
+        'username',
+        'email',
+    ];
+
+    protected $columns = [
+        'username',
+        'email',
+    ];
+
+    protected $buttons = [
+        'edit' => [
+            'text'       => 'Edit',
+            'attributes' => [
+                'href' => '/admin/users/edit/{{ entry.id }}',
             ]
-        );
-    }
+        ],
+    ];
 
-    /**
-     * Set up the table filters.
-     */
-    protected function setUpFilters()
-    {
-        $this->setFilters(
-            [
-                'email',
-            ]
-        );
-    }
-
-    /**
-     * Set up the table columns.
-     */
-    protected function setUpColumns()
-    {
-        $this->setColumns(
-            [
-                [
-                    'heading' => 'Title Here',
-                    'value'   => function (EntryInterface $entry) {
-
-                            if (!$profile = $entry->profile) {
-                                return null;
-                            }
-
-                            return $profile->display_name;
-                        }
-                ],
-                'username',
-                'email.link',
-                'last_login_at.valueAndDiffForHumans',
-                [
-                    'value' => function (UserInterface $entry) {
-
-                            $class = null;
-                            $title = null;
-
-                            $block      = $entry->getBlock();
-                            $activation = $entry->getActivation();
-
-                            if (!$activation instanceof ActivationInterface or !$activation->isComplete()) {
-
-                                $class = 'default';
-                                $title = 'module::ui.inactive';
-                            }
-
-                            if ($activation instanceof ActivationInterface and $activation->isComplete()) {
-
-                                $class = 'success';
-                                $title = 'module::ui.active';
-                            }
-
-                            if ($block instanceof BlockInterface) {
-
-                                $class = 'danger';
-                                $title = 'module::ui.blocked';
-                            }
-
-                            return '<span class="label label-' . $class . '">' . trans($title) . '</span>';
-                        },
-                ],
-                [
-                    'value' => function ($entry) {
-
-                            $active = date('Y-m-d H:i:s', strtotime('-15 minutes'));
-
-                            $class = 'default';
-                            $title = 'Offline';
-
-                            if ($entry->last_activity_at > $active) {
-
-                                $class = 'success';
-                                $title = 'Online';
-                            }
-
-                            return '<span class="label label-' . $class . '">' . trans($title) . '</span>';
-                        },
-                ],
-            ]
-        );
-    }
-
-    /**
-     * Set up table buttons.
-     */
-    protected function setUpButtons()
-    {
-        $this->setButtons(
-            [
-                'edit',
-            ]
-        );
-    }
-
-    /**
-     * Set up the table actions.
-     */
-    protected function setUpActions()
-    {
-        $this->setActions(
-            [
-                'success' => [
-                    'title'   => 'module.users::button.activate',
-                    'handler' => 'ActivateUsersTableAction@handle',
-                    'enabled' => true
-                ],
-            ]
-        );
-    }
+    protected $actions = [];
 }
  
