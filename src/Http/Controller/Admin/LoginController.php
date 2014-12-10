af2a2044b5120cb21bb7303469c1d7de43feebb2
Ryan Thompson <service@aiwebsystems.com>
2014-12-09 23:18:18 -0600

    Cleaning up a bit

--- src/Http/Controller/Admin/LoginController.php
+++ src/Http/Controller/Admin/LoginController.php
@@ -1,6 +1,5 @@
 <?php namespace Anomaly\Streams\Addon\Module\Users\Http\Controller\Admin;
 
-use Anomaly\Streams\Addon\Module\Users\User\UserModel;
 use Anomaly\Streams\Platform\Http\Controller\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
@@ -23,6 +22,7 @@ class LoginController extends AdminController
      */
     public function login()
     {
+
         /**
          * If the user is already logged in
          * then send them to their home page.
@@ -43,51 +43,16 @@ class LoginController extends AdminController
      * @param Redirector $redirect
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function attempt(
-        Request $request,
-        Redirector $redirect
-    ) {
-        /**
-         * Attempt to login and start a session. Otherwise
-         * an exception should be thrown and added to messages.
-         */
-        try {
+    public function attempt(Request $request, Redirector $redirect)
+    {
+        $email    = $request->get('email');
+        $password = $request->get('password');
+        $remember = ($request->get('remember'));
 
-            if ($user = app('streams.auth')->authenticate($request->all()) and $user instanceof UserModel) {
+        if (app('auth')->attempt(compact('email', 'password'), $remember) or app('auth')->user()) {
 
-                app('streams.auth')->login($user, ($request->get('remember') == 'on'));
-
-                return $redirect->intended(preference('module.users::home_page', 'admin/dashboard'));
-            }
-        } catch (UserNotFoundException $e) {
-
-            // The user was not found.
-            app('streams.messages')->add('error', 'module.users::error.user_not_found');
-        } catch (LoginRequiredException $e) {
-
-            // Input validation failed.
-            app('streams.messages')->add('error', 'module.users::error.login_required');
-        } catch (PasswordRequiredException $e) {
-
-            // Input validation failed.
-            app('streams.messages')->add('error', 'module.users::error.password_required');
-        } catch (\Exception $e) {
-
-            // If debug - bitch.
-            if (config('app.debug')) {
-
-                throw new \Exception($e->getMessage());
-            }
-
-            // There was some other error.
-            app('streams.messages')->add('error', 'error.generic');
+            return $redirect->intended(preference('module.users::home_page', 'admin/dashboard'));
         }
-
-        /**
-         * If we have made it this far then we've failed.
-         * Flash the messages and redirect back to login.
-         */
-        app('streams.messages')->flash();
 
         return $redirect->to('admin/login');
     }
