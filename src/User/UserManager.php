f66dd059573b8da5789e42f9c499581b133bbc9a
Ryan Thompson <service@aiwebsystems.com>
2014-12-09 23:20:38 -0600

    New user manager

---
+++ src/User/UserManager.php
@@ -0,0 +1,17 @@
+<?php namespace Anomaly\Streams\Addon\Module\Users\User;
+
+use Laracasts\Commander\CommanderTrait;
+
+class UserManager
+{
+
+    use CommanderTrait;
+
+    public function create(array $credentials, $activate = false)
+    {
+        $user = $this->execute('\Anomaly\Streams\Addon\Module\Users\User\Command\CreateUserCommand', $credentials);
+
+        return $user;
+    }
+}
+ 
