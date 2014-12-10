e11436ba61ac44d7a1f73f083d91de318b87a87b
Ryan Thompson <service@aiwebsystems.com>
2014-12-09 23:18:01 -0600

    Bind the model too

--- resources/config/config.php
+++ resources/config/config.php
@@ -6,7 +6,6 @@ return [
      * User management configuration.
      */
     'users' => [
-        'model' => 'Anomaly\Streams\Addon\Module\Users\User\UserModel',
         'repository' => 'Anomaly\Streams\Addon\Module\Users\User\UserRepository',
     ],
 ];
