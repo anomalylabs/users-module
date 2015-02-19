a50724e0e779a72683130552d0695ec6998e7f03
Ryan Thompson <service@aiwebsystems.com>
2015-02-19 11:59:23 -0600

    Silly

--- resources/lang/en/permission.php
+++ resources/lang/en/permission.php
@@ -5,7 +5,7 @@ return [
     'create_user'  => 'Create User',
     'edit_user'    => 'Edit User',
     'delete_user'  => 'Delete User',
-    'create_role'  => 'Create RoleInterface',
-    'edit_role'    => 'Edit RoleInterface',
-    'delete_role'  => 'Delete RoleInterface',
+    'create_role'  => 'Create Role',
+    'edit_role'    => 'Edit Role',
+    'delete_role'  => 'Delete Role',
 ];
