a0f1cf325da247a1bee2ef722c9e427102e0f7cb
Ryan Thompson <service@aiwebsystems.com>
2015-03-27 21:56:07 -0500

    Progress

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -4,6 +4,7 @@ return [
     'edit_admin_error'              => 'Admin can not be edited.',
     'block_users_success'           => ':count user(s) were blocked successfully.',
     'unblock_users_success'         => ':count user(s) were unblocked successfully.',
+    'activate_users_success'        => ':count user(s) were activated successfully.',
     'save_user_permissions_success' => 'User permissions for :username have been saved successfully.',
     'save_role_permissions_success' => 'Role permissions for :slug have been saved successfully.'
 ];
