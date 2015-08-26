b409a9a920a53839e65902f98de33b9ff3fbf1d8
Marco Grüter <marco.grueter@healthworld.ch>
2015-08-26 10:52:36 +0200

    german translation

---
+++ resources/lang/de/setting.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'allow_registration' => [
+        'label'        => 'Erlaube Registrierungen',
+        'instructions' => 'Soll es Benutzern erlaubt sein, sich über die Website registrieren zu können?',
+        'text'         => 'Ja, erlaube die Benutzer-Registrierung'
+    ],
+    'activation_mode'    => [
+        'label'        => 'Aktivierungsmodus',
+        'instructions' => 'Wie sollen Benutzer nach der Registrierung aktiviert werden?',
+        'option'       => [
+            'manual'    => 'Ein Administrator muss die Benutzer manuell aktivieren.',
+            'email'     => 'Sende eine E-Mail mit einer Aktivierungsbestätigung an den Benutzer.',
+            'automatic' => 'Aktiviere den Benutzer nach der Registrierung automatisch.'
+        ]
+    ],
+    'profile_visibility' => [
+        'label'        => 'Sichtbarkeit des Profils',
+        'instructions' => 'Definieren Sie, wer Benutzerprofile auf der öffentlichen Website einsehen kann.',
+        'option'       => [
+            'everyone' => 'Jeder kann öffentliche Profile einsehen.',
+            'owner'    => 'Nur der Besitzer des Profils kann sein Profil einsehen.',
+            'disabled' => 'Deaktiviere diese Funktion.',
+            'users'    => 'Jeder angemeldete Benutzer kann die Profile der anderen Benutzer einsehen.'
+        ]
+    ]
+];
---
+++ resources/lang/de/view.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'online'    => 'Online',
+    'inactive'  => 'Inaktiv',
+    'suspended' => 'Suspendiert'
+];
