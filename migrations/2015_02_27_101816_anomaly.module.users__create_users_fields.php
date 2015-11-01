f3557f75d75906c41d431947a45c2a4173ef9f3a
Ryan Thompson <ryan@pyrocms.com>
2015-10-31 22:29:24 -0500

    Sneak these in real quick - implement later

--- migrations/2015_02_27_101816_anomaly.module.users__create_users_fields.php
+++ migrations/2015_02_27_101816_anomaly.module.users__create_users_fields.php
@@ -18,56 +18,58 @@ class AnomalyModuleUsersCreateUsersFields extends Migration
      * @var array
      */
     protected $fields = [
-        'email'            => 'anomaly.field_type.email',
-        'username'         => [
+        'email'                      => 'anomaly.field_type.email',
+        'username'                   => [
             'type'   => 'anomaly.field_type.slug',
             'config' => [
                 'type' => '_'
             ]
         ],
-        'password'         => [
+        'password'                   => [
             'type'   => 'anomaly.field_type.text',
             'config' => [
                 'type' => 'password'
             ]
         ],
-        'avatar'           => [
+        'avatar'                     => [
             'type'   => 'anomaly.field_type.file',
             'config' => [
                 'disk'   => 'public',
                 'folder' => 'Users Module/Avatars'
             ]
         ],
-        'remember_token'   => 'anomaly.field_type.text',
-        'ip_address'       => 'anomaly.field_type.text',
-        'last_login_at'    => 'anomaly.field_type.datetime',
-        'last_activity_at' => 'anomaly.field_type.datetime',
-        'permissions'      => 'anomaly.field_type.checkboxes',
-        'display_name'     => 'anomaly.field_type.text',
-        'first_name'       => 'anomaly.field_type.text',
-        'last_name'        => 'anomaly.field_type.text',
-        'name'             => 'anomaly.field_type.text',
-        'reset_code'       => 'anomaly.field_type.text',
-        'activation_code'  => 'anomaly.field_type.text',
-        'activated'        => [
+        'remember_token'             => 'anomaly.field_type.text',
+        'ip_address'                 => 'anomaly.field_type.text',
+        'last_login_at'              => 'anomaly.field_type.datetime',
+        'last_activity_at'           => 'anomaly.field_type.datetime',
+        'permissions'                => 'anomaly.field_type.checkboxes',
+        'display_name'               => 'anomaly.field_type.text',
+        'first_name'                 => 'anomaly.field_type.text',
+        'last_name'                  => 'anomaly.field_type.text',
+        'name'                       => 'anomaly.field_type.text',
+        'reset_code'                 => 'anomaly.field_type.text',
+        'reset_code_expires_at'      => 'anomaly.field_type.datetime',
+        'activation_code'            => 'anomaly.field_type.text',
+        'activation_code_expires_at' => 'anomaly.field_type.datetime',
+        'activated'                  => [
             'type'   => 'anomaly.field_type.boolean',
             'config' => [
                 'default_value' => false
             ]
         ],
-        'enabled'          => [
+        'enabled'                    => [
             'type'   => 'anomaly.field_type.boolean',
             'config' => [
                 'default_value' => true
             ]
         ],
-        'slug'             => [
+        'slug'                       => [
             'type'   => 'anomaly.field_type.slug',
             'config' => [
                 'slugify' => 'name'
             ]
         ],
-        'roles'            => [
+        'roles'                      => [
             'type'   => 'anomaly.field_type.multiple',
             'config' => [
                 'related' => 'Anomaly\UsersModule\Role\RoleModel'
