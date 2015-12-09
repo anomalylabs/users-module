f78263dadf83b39ff1991b400db3571e77ec1ec8
Ryan Thompson <service@aiwebsystems.com>
2015-12-09 17:11:53 -0600

    Allow null

--- src/UsersModulePlugin.php
+++ src/UsersModulePlugin.php
@@ -2,6 +2,7 @@
 
 use Anomaly\Streams\Platform\Addon\Plugin\Plugin;
 use Anomaly\Streams\Platform\Support\Decorator;
+use Anomaly\UsersModule\Role\Command\GetRole;
 use Anomaly\UsersModule\User\Command\GetLogoutPath;
 use Anomaly\UsersModule\User\Command\GetUser;
 
@@ -26,14 +27,14 @@ class UsersModulePlugin extends Plugin
         return [
             new \Twig_SimpleFunction(
                 'user',
-                function ($identifier) {
+                function ($identifier = null) {
                     return (new Decorator())->decorate($this->dispatch(new GetUser($identifier)));
                 }
             ),
             new \Twig_SimpleFunction(
                 'role',
                 function ($identifier) {
-                    return (new Decorator())->decorate($this->dispatch(new GetUser($identifier)));
+                    return (new Decorator())->decorate($this->dispatch(new GetRole($identifier)));
                 }
             ),
             new \Twig_SimpleFunction(
