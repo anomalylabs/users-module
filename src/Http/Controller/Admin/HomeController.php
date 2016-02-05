452c5ce4e9843f62d421f7907a87b4c034baa9fd
Ryan Thompson <ryan@pyrocms.com>
2016-02-05 13:15:05 -0600

    redirect to HOME

--- src/Http/Controller/Admin/HomeController.php
+++ src/Http/Controller/Admin/HomeController.php
@@ -1,6 +1,7 @@
 <?php namespace Anomaly\UsersModule\Http\Controller\Admin;
 
 use Anomaly\Streams\Platform\Http\Controller\AdminController;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Routing\Redirector;
 
 /**
@@ -18,10 +19,11 @@ class HomeController extends AdminController
      * Redirect to the users home page.
      *
      * @param Redirector $redirect
+     * @param Repository $config
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function index(Redirector $redirect)
+    public function index(Redirector $redirect, Repository $config)
     {
-        return $redirect->to('admin/dashboard');
+        return $redirect->to($config->get('anomaly.module.users::paths.cp_home', 'admin/dashboard'));
     }
 }
