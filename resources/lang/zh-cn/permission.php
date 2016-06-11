9016088fb8ebb7518421f02089f090142faad773
GoonerRay <rayyang88@gmail.com>
2016-06-11 10:49:52 +0800

    Add Simplified Chinese localization

---
+++ resources/lang/zh-cn/permission.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'users'    => [
+        'name'   => '用户',
+        'option' => [
+            'read'   => '可读取用户资料。',
+            'write'  => '可建立与编辑用户资料。',
+            'delete' => '可删除用户资料。',
+        ],
+    ],
+    'roles'    => [
+        'name'   => '角色',
+        'option' => [
+            'read'   => '可读取角色资料。',
+            'write'  => '可建立与编辑角色资料。',
+            'delete' => '可删除角色资料。',
+        ],
+    ],
+    'fields'   => [
+        'name'   => '字段',
+        'option' => [
+            'manage' => '可管理自定义字段。',
+        ],
+    ],
+    'settings' => [
+        'name'   => '设定',
+        'option' => [
+            'manage' => '可管理 addon 设定。',
+        ],
+    ],
+];
---
+++ resources/lang/zh-cn/warning.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'modify_admin_user' => 'Admin 用户不能被更改。',
+    'modify_admin_role' => '角色 admin 不能被更改。'
+];
