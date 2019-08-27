26255fbdbade13e1a01ce074a43929fec76b1730
alp <alp@visiosoft.com.tr>
2019-08-27 17:32:03 +0300

    Turkish translations added

---
+++ resources/lang/tr/addon.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "title" => "Kullanıcılar",
+    "name" => "Kullanıcılar Modülü",
+    "description" => "Kullanıcı rolleri ve izinlerini yönetin.",
+    "section" => [
+        "users" => "Kullanıcılar",
+        "roles" => "Roller",
+        "fields" => "Alanlar",
+    ],
+];
---
+++ resources/lang/tr/field.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+    "name" => [
+        "name" => "isim",
+        "instructions" => [
+            "roles" => "Bu rol için kısa bir açıklayıcı ad belirtin."
+        ],
+
+    ],
+    "description" => [
+        "name" => "Açıklama",
+        "instructions" => [
+            "roles" => "Bu rolü kısaca açıklayınız."
+        ],
+
+    ],
+    "first_name" => [
+        "name" => "İsim",
+        "instructions" => "Kullanıcının gerçek ilk adını belirtin.",
+
+    ],
+    "last_name" => [
+        "name" => "Soyadı",
+        "instructions" => "User's gerçek soyadını belirtin.",
+
+    ],
+    "display_name" => [
+        "name" => "Ekran adı",
+        "instructions" => "Kullanıcı'larin genel olarak görüntülenebilecek adını belirtin.",
+
+    ],
+    "username" => [
+        "name" => "Kullanıcı adı",
+        "instructions" => "Kullanıcı adı,
+      bu kullanıcıyı benzersiz bir şekilde tanımlamak ve görüntülemek için kullanılır.",
+
+    ],
+    "email" => [
+        "name" => "E-posta",
+        "instructions" => "E-posta, giriş yapmak için kullanılır.",
+
+    ],
+    "password" => [
+        "name" => "Parola",
+        "instructions" => "User's güvenli şifresini belirtin.",
+        "impersonate" => "Devam etmek için lütfen mevcut şifrenizi onaylayın.",
+
+    ],
+    "confirm_password" => [
+        "name" => "Şifreyi Onayla",
+
+    ],
+    "slug" => [
+        "name" => "sümüklüböcek",
+        "instructions" => [
+            "roles" => "Sümüklü böcek bu rolü benzersiz olarak tanımlamak için kullanılır."
+        ],
+
+    ],
+    "roles" => [
+        "name" => "Roller",
+        "instructions" => "Kullanıcının hangi rollere ait olduğunu belirtin.",
+
+    ],
+    "permissions" => [
+        "name" => "İzinler",
+
+    ],
+    "last_activity_at" => [
+        "name" => "son Aktivite",
+
+    ],
+    "activated" => [
+        "name" => "Aktive",
+        "label" => "Bu kullanıcı aktif mi?",
+        "instructions" => "Etkinleştirilmediği sürece kullanıcı giriş yapamaz.",
+
+    ],
+    "enabled" => [
+        "name" => "Etkin",
+        "label" => "Bu kullanıcı etkin mi?",
+        "instructions" => "Kullanıcı,
+      devre dışı bırakılırsa giriş yapamaz veya etkinleştiremez.",
+
+    ],
+    "activation_code" => [
+        "name" => "Aktivasyon kodu",
+
+    ],
+    "reset_code" => [
+        "name" => "Sıfırlama kodu",
+
+    ],
+    "remember_me" => [
+        "name" => "Beni Hatırla",
+
+    ],
+    "status" => [
+        "name" => "durum",
+        "option" => [
+            "active" => "Aktif        ",
+            "inactive" => "etkisiz        ",
+            "disabled" => "engelli",
+
+        ],
+
+    ],
+
+];
